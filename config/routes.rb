abcc16e043654bd06fb17e861cdfdec756d26903
John Hayes <JohnnyHayes7@gmail.com>
2019-07-31 11:19:12 -0400

    built new season path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "static#welcome"
   resources :teams, only:[:index]
   resources :administrators, only:[:show, :new, :create]
+  resources :seasons, only:[:new]
   # get "/admin" => "administrator#admin"
 end
