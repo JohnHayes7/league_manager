7a23158ce9174e9d12b4e6d20e44e3dfae307e1a
John Hayes <JohnnyHayes7@gmail.com>
2019-08-06 09:43:20 -0400

    Can successfully create referee and link to referee show page

--- app/controllers/referees_controller.rb
+++ app/controllers/referees_controller.rb
@@ -4,4 +4,27 @@ class RefereesController < ApplicationController
         @ref = Referee.new
     end
 
+    def create
+        ref = Referee.new(ref_params)
+        if ref.save
+            binding.pry
+            redirect_to referee_path(ref)
+        else
+            flash[:error] = "All fields required to create a new referee account"
+            render :new
+        end
+    end
+
+    def show
+        @ref = Referee.find(params[:id])
+    end
+
+
+
+    private
+
+    def ref_params
+        params.require(:referee).permit(:name, :email, :phone, :password)
+    end
+
 end
--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,4 +1,6 @@
 class Referee < ApplicationRecord
     has_many :matches
     has_many :match_notes
+
+    has_secure_password
 end
