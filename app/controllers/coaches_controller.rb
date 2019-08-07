cd949dc01cde57be61714e4a13e985e982db0dc2
John Hayes <JohnnyHayes7@gmail.com>
2019-08-07 08:21:35 -0400

    can create new coach and linkto/display coach show page

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -4,4 +4,25 @@ class CoachesController < ApplicationController
         @coach = Coach.new
     end
 
+    def create
+        coach = Coach.new(coach_params)
+        if coach.save
+
+            redirect_to coach_path(coach)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @coach = Coach.find(params[:id])
+    end
+
+
+    private
+
+    def coach_params
+        params.require(:coach).permit(:name, :email, :phone, :password_digest)
+    end
+
 end
--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,8 @@
 class Coach < ApplicationRecord
     has_many :teams  
     has_many :matches, through: :teams 
+
+    def last_name
+        self.name.split[1]
+    end
 end
