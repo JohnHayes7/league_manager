fd5fb65920d1faf930bc6f5b65622c77fa078091
John Hayes <JohnnyHayes7@gmail.com>
2019-08-08 11:51:27 -0400

    Can successfully login and redirect to appropriate page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,24 @@ class SessionsController < ApplicationController
     end
 
     def create
-       @user = Administrator.find(params[:session][:email])
+       @user = Administrator.find_by(email: params[:session][:email]) || @user = Referee.find_by(email: params[:session][:email]) || @user = Coach.find_by(email: params[:session][:email])
+        
+       if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            if @user.admin?
+                redirect_to administrator_path(@user)
+
+            elsif @user.referee?
+                redirect_to referee_path(@user)
+
+            else 
+                redirect_to coach_path(@user)
+            end
+       else
+
+            render :new
+       end
+
        
     end
 
--- app/models/coach.rb
+++ app/models/coach.rb
@@ -2,6 +2,8 @@ class Coach < ApplicationRecord
     has_many :teams  
     has_many :matches, through: :teams 
 
+    has_secure_password
+
     def last_name
         self.name.split[1]
     end
