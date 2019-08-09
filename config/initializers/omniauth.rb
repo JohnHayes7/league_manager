0bc9543cb96a539bc5d5e62bdf32162b08748033
John Hayes <JohnnyHayes7@gmail.com>
2019-08-09 13:39:21 -0400

    debugging omniauth-google login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true, {
-        redirect_uri:"http://localhost:3000/auth/google_oauth2/callback"
-      }
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
   end
