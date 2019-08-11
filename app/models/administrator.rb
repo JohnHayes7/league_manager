abddfabd35c8c89ec5d65a9df0c4b9a740a5a9e9
John Hayes <JohnnyHayes7@gmail.com>
2019-08-11 14:59:39 -0400

    Added validation and errors to Administrators

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,5 +1,8 @@
 class Administrator < ApplicationRecord
-
+    validates :name, :presence => {:message => "You Must Provide a Name to Create and Account"}
+    validates :email, :presence => {:message => "You Must Provide a email to Create and Account"},
+                      :uniqueness => {:message => "This Email Address is already associated to an account.  Please Login"}
+    validates :password, :length => {in: 6..10, :message => "Passwords must between 6 and 10 characters long"}
     has_secure_password
 
     def first_name
