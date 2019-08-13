6bea177dca33e4e87f1c12389b76025674608e43
John Hayes <JohnnyHayes7@gmail.com>
2019-08-13 15:52:12 -0400

    Added partial and refactored location show page

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,5 +10,17 @@ class Location < ApplicationRecord
     validates :city, :presence => {:message => "Must Be Provided to create a new Location"}
     validates :state, :presence => {:message => "Must Be Provided to create a new Location"}
     validates :zip_code,  :presence => {:message => "Must Be Provided to create a new Location"}
+   
     
+    def has_matches?
+        !self.matches.empty?
+    end
+
+    def past_matches
+        self.matches.where("date < ?", Time.zone.today)
+    end
+
+    def upcoming_matches
+        self.matches.where("date >= ?", Time.zone.today)
+    end
 end
