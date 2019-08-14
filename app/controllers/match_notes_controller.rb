a25e9177ea1960ae97e042c53446354a20bf2bbe
John Hayes <JohnnyHayes7@gmail.com>
2019-08-14 09:25:56 -0400

    added flash messages to match_notes

--- app/controllers/match_notes_controller.rb
+++ app/controllers/match_notes_controller.rb
@@ -13,38 +13,49 @@ class MatchNotesController < ApplicationController
     end
 
     def create
-       
-        note = MatchNote.new(match_note_params)
-        note.referee_id = params[:referee_id]
         match = Match.find(params[:match_note][:match_id])
-        match.goals_update(params)
-        
-        if note.save
+        if ref_logged_in && belongs_to_ref(match)
+            note = MatchNote.new(match_note_params)
+            note.referee_id = params[:referee_id]
+            
+            match.goals_update(params)
+            
+            if note.save
 
-            redirect_to competition_match_path(match.competition_id, match)
+                redirect_to competition_match_path(match.competition_id, match)
+            end
+        else
+            flash[:error] = "Only the Referee who officiated this game can add match notes"
+            redirect_to login_path
         end
     end
 
     def edit
         @match = Match.find(params[:match_id])
         @comp = Competition.find(@match.competition_id)
-        if ref_logged_in? && @match.belongs_to_ref
+        if ref_logged_in? && belongs_to_ref(@match)
             @note = MatchNote.find(params[:id])
             @ref = Referee.find(params[:referee_id])
         else
+            flash[:error] = "Only the Referee who officiated this game can edit match notes"
             redirect_to competition_match_path(@match.competition_id, @match)
         end
     end
 
     def update
         @match = Match.find(params[:match_id])
-        @note = MatchNote.find(params[:id])
-        @match.goals_update(params)
-       
-        @note.update(match_note_params)
-        @note.save
+        if ref_logged_in? && belongs_to_ref(@match)
+            @note = MatchNote.find(params[:id])
+            @match.goals_update(params)
+        
+            @note.update(match_note_params)
+            @note.save
 
-        redirect_to competition_match_path(@match.competition_id, @match)
+            redirect_to competition_match_path(@match.competition_id, @match)
+        else
+            flash[:error] = "Only the Referee who officiated this game can edit match notes"
+            redirect_to competition_match_path(@match.competition_id, @match)
+        end
     end
     
 
