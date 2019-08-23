ea3c46e02848db82524c16bf27b38772563bb7df
John Hayes <JohnnyHayes7@gmail.com>
2019-08-23 13:36:14 -0400

    DRYed up admin controller

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,42 +1,29 @@
 class AdministratorsController < ApplicationController
+    before_action :admin_login, except: [:index, :edit]
 
     def index
         @admins = Administrator.order(:name)
     end
 
     def new
-        if admin_logged_in?
-            @admin = Administrator.new
-        else
-            flash[:error] = "You must be an administrator to continue"
-            redirect_to login_path
-        end
+        @admin = Administrator.new
     end
     
     def create
-        if admin_logged_in?
-            @admin = Administrator.new(admin_params)
-            if @admin.save
-
-                redirect_to administrator_path(@admin)
-            else
-                render :new
-            end
+        @admin = Administrator.new(admin_params)
+        if @admin.save
+            redirect_to administrator_path(@admin)
+        else
+            render :new
         end
     end
 
     def show
-        if admin_logged_in?
-            @admin = Administrator.find_by(:id => params[:id])
-            @teams = Team.order(:name)
-            @coaches = Coach.order(:name)
-            @seasons = Season.order(year: :desc)
-            @referees = Referee.order(:name)
-        else
-            flash[:error] = "You must be an administrator to continue"
-            redirect_to login_path
-        end
-        
+        @admin = Administrator.find_by(:id => params[:id])
+        @teams = Team.order(:name)
+        @coaches = Coach.order(:name)
+        @seasons = Season.order(year: :desc)
+        @referees = Referee.order(:name)
     end
 
     def edit
@@ -51,20 +38,23 @@ class AdministratorsController < ApplicationController
 
     def destroy
         @admin = Administrator.find(params[:id])
-        if admin_logged_in?
-            @admin.destroy
-
-            redirect_to administrators_path
-        else
-            flash[:error] = "You must be an administrator to continue"
-            redirect_to login_path
-        end
+        @admin.destroy
+        redirect_to administrators_path
     end
+
+    
 
     private
 
     def admin_params
         params.require(:administrator).permit(:name, :email, :password)
+    end
+
+    def admin_login
+        unless admin_logged_in?
+            flash[:error] = "You must be an administrator to continue"
+            redirect_to login_path
+        end
     end
     
 end
