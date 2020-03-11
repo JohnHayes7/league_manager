05e48dfe74ed650d840e1d229fd97de32fbfc870
John Hayes <JohnnyHayes7@gmail.com>
2020-03-10 22:01:24 -0400

    debugging db issues

---
+++ db/migrate/20200311014723_remove_attrs_from_teams.rb
@@ -0,0 +1,7 @@
+class RemoveAttrsFromTeams < ActiveRecord::Migration[5.2]
+  def change
+    # remove_column :teams, :wins
+    # remove_column :teams, :loses
+    # remove_column :teams, :draws
+  end
+end
